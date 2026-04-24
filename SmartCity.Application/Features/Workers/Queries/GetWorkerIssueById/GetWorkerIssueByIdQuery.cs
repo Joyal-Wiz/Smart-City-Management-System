d3a2c80d81993bcd6c510bcaff562a69797d1ab7
J-o-y-a-l <joyaljose674@gmail.com>
2026-04-24 13:08:21 +0530

    Add GetWorkerIssueById endpoint with MediatR query and handler

---
+++ SmartCity.Application/Features/Workers/Queries/GetWorkerIssueById/GetWorkerIssueByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCity.Application.Features.Workers.Queries.GetWorkerIssueById
+{
+    internal class GetWorkerIssueByIdQuery
+    {
+    }
+}
